e8a9fc85e09044b08a70b014fd80ef5771e84e30
pjruddy1 <ruddypj.pr@gmail.com>
2020-01-28 20:19:29 -0500

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVC5Employees.Models;
+
+namespace MVC5Employees.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        [HttpPost]
+        public ActionResult AddEmp(int empId, string empName, int empSal)
+        {
+            Employee e = new Employee()
+            {
+                Id = empId,
+                Name = empName,
+                Salary = empSal
+            };
+            return View("EmpConfirm", e);
+        }
+
+        public ActionResult AddEmp()
+        {
+            return View("NewEmpForm");
+        }
+
+        public ActionResult GrossPayCalculator()
+        {
+            return View();
+        }
+
+        public ActionResult mainOfficeEmployees()
+        {
+            return View();
+        }
+    }
+}
